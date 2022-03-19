6a1d4f95c6da80a621ebd46510f4158ca799f53b
hcumbicusr <hcumbicusr@gmail.com>
2022-03-18 23:48:06 -0500

    vercel

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,6 +1,10 @@
 // const store = require('../../../store/dummy');
-// const store = require('../../../store/mysql');
-const store = require('../../../store/remote-mysql');
+const config = require('../../../config');
+if ( config.remoteDB === true) {
+    const store = require('../../../store/remote-mysql');
+} else {
+    const store = require('../../../store/mysql');
+}
 const ctrl = require('./controller');
 
 module.exports = ctrl(store);
